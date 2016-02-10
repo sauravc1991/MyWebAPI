7627dad0dbe6cc67abb256031ebfe70d53392775
Saurav Chatterjee <saurav.chatterjee91@gmail.com>
2016-02-10 22:33:44 +0530

    #comment: Added full CRUD functionality Initial commit

---
+++ MyWebAPI/Controllers/DataController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using MyWebAPI.Models;
+
+namespace MyWebAPI.Controllers
+{
+    public class DataController : ApiController
+    {
+        private SampleEntities db = new SampleEntities();
+
+        // GET api/Data
+        public IEnumerable<Student> GetStudents()
+        {
+            return db.Students.AsEnumerable();
+        }
+
+        // GET api/Data/5
+        public Student GetStudent(int id)
+        {
+            Student student = db.Students.Find(id);
+            if (student == null)
+            {
+                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
+            }
+
+            return student;
+        }
+
+        // PUT api/Data/5
+        public HttpResponseMessage PutStudent(int id, Student student)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            }
+
+            if (id != student.ID)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+
+            db.Entry(student).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+
+        // POST api/Data
+        public HttpResponseMessage PostStudent(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Students.Add(student);
+                db.SaveChanges();
+
+                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created, student);
+                response.Headers.Location = new Uri(Url.Link("DefaultApi", new { id = student.ID }));
+                return response;
+            }
+            else
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            }
+        }
+
+        // DELETE api/Data/5
+        public HttpResponseMessage DeleteStudent(int id)
+        {
+            Student student = db.Students.Find(id);
+            if (student == null)
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+
+            db.Students.Remove(student);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, student);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MyWebAPI/DataSvc.js
@@ -0,0 +1,86 @@
+﻿var myApp = angular.module('myApp', []);
+myApp.controller('myCtrl', ['$scope', '$http', function ($scope, $http) {
+  console.log("controller hit");
+
+  getStudents();
+
+  //Function to get Students List
+  function getStudents() {
+    $http({
+      method: 'GET',
+      url: '/api/data'
+    }).
+      success(function (data) {
+        $scope.students = data;
+      });
+  };
+
+  //Function to add a new student record
+  $scope.addStudent = function() {
+    $http({
+        method: 'POST',
+        url: '/api/data',
+        data: JSON.stringify($scope.newStudent),
+        headers: { 'Content-Type': 'application/JSON' }
+      }).
+      success(function(data) {
+        window.alert("The Student Saved Successfully!!!");
+        getStudents();
+      })
+      .error(function(error) {
+        window.alert('Unable to create a student: ' + error.message);
+      });
+  };
+
+  //Function to delete a student record
+  $scope.delStudent = function(id) {
+    console.log(id);
+    $http({
+        method: 'DELETE',
+        url: '/api/data/' + id
+      }).
+      success(function(data) {
+        window.alert("The Student deleted Successfully!!!");
+        getStudents();
+      })
+      .error(function(error) {
+        window.alert('Unable to delete the student: ' + error.message);
+      });
+  };
+
+  //Function to Populate Edit Form
+  $scope.populateForm = function(id) {
+    $http({
+        method: 'GET',
+        url: '/api/data/' + id
+      }).
+      success(function(data) {
+        $scope.newStudent = data;
+      });
+  };
+
+  //Function to edit a student record
+  $scope.updateStudent = function (id) {
+    debugger;
+    $http({
+      method: 'PUT',
+      url: '/api/data/' + id,
+      data: JSON.stringify($scope.newStudent)
+    }).
+    success(function(data) {
+      window.alert('Student record updated successfully');
+        getStudents();
+      });
+
+  };
+
+  $scope.checkNull = function(students) {
+    if (students.length > 0) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+
+}]);
